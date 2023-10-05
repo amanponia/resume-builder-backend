d981d2b0437362a11a8c91e30c2d201116544639
Aman Chaudhary <amanchaudhary@uimatic.com>
2023-10-05 16:45:36 +0530

    feat: setup JWT auth and login related APIs

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('fallback', function () {
+    return response()->json('Contact Aman to access this resource.', 404);
+})->name('fallback');
